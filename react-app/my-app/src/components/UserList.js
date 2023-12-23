2e221bbfec691ed085e21260787c356efd2ad2d8
ChienDC <doancaochien.1994@gmail.com>
2023-12-22 18:58:40 -0800

    alo

---
+++ react-app/my-app/src/components/UserList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import UserList from './components/UserList';
+
+export default function MyApp(){
+    return (
+        <div className="App">
+            <UserList />
+        </div>
+    );
+}
